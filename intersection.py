afccae93d3ded56687aea6ed1064080ac3aebd8d
Flauver <thedarkwhite@126.com>
2025-04-08 20:05:32 +0800

    更改交集算法，新增五千词条

--- intersection.py
+++ intersection.py
@@ -1,16 +1,23 @@
+from encodings.punycode import T
 import os
 
-dicts = []
+dicts = {}
+ls = set()
 for file in [f for f in os.listdir("纯词库") if os.path.isfile(f"纯词库/{f}")]:
-	dict = set()
 	with open(f"纯词库/{file}", 'r', encoding='utf-8') as f:
+		file_is_lansan = False
+		if file == '蓝三.txt':
+			file_is_lansan = True
 		for line in f:
-			dict.add(line.strip())
-	dicts.append(dict)
+			word = line.strip()
+			if word in dicts:
+				dicts[word] += 1
+			else:
+				dicts[word] = 1
+			if file_is_lansan:
+				ls.add(word)
 
-result = dicts[0]
-for dict in dicts[1:]:
-	result = result & dict
+result = [x[0] for x in dicts.items() if x[1] > 3 and x[0] in ls]
 
 with open('交集.txt', 'w', encoding='utf-8') as f:
 	f.write('\n'.join(result))
--- sort.py
+++ sort.py
@@ -1,7 +1,7 @@
 dict = set()
-with open('Dictlinglong.txt', 'r', encoding='utf-8') as f:
+with open('DictLinglong.txt', 'r', encoding='utf-8') as f:
 	for line in f:
 		dict.add(line.strip())
 
-with open('Dictlinglong.txt', 'w', encoding='utf-8') as f:
+with open('DictLinglong.txt', 'w', encoding='utf-8') as f:
 	f.write('\n'.join(sorted(dict)))
