01057aa118882f5aff374ba447e48a402686c602
Flauver <thedarkwhite@126.com>
2025-04-09 17:36:43 +0800

    删除一些两字复合词

--- sort.py
+++ sort.py
@@ -5,3 +5,14 @@ with open('DictLinglong.txt', 'r', encoding='utf-8') as f:
 
 with open('DictLinglong.txt', 'w', encoding='utf-8') as f:
 	f.write('\n'.join(sorted(dict)))
+	
+origindict = set()
+with open('交集.txt', 'r', encoding='utf-8') as f:
+	for line in f:
+		origindict.add(line.strip())
+
+with open('增.txt', 'w', encoding='utf-8') as f:
+	f.write('\n'.join(sorted(dict.difference(origindict))))
+
+with open('减.txt', 'w', encoding='utf-8') as f:
+	f.write('\n'.join(sorted(origindict.difference(dict))))
