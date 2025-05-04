2bc1483d94e8258e00cc5b18ef0c35a4aa7b778d
Songchen Tan <i@tansongchen.com>
2025-05-03 21:04:26 -0400

    增加并集功能

--- intersection.py
+++ intersection.py
@@ -3,6 +3,7 @@ import os
 
 dicts = {}
 ls = set()
+union = set()
 for file in [f for f in os.listdir("纯词库") if os.path.isfile(f"纯词库/{f}")]:
 	with open(f"纯词库/{file}", 'r', encoding='utf-8') as f:
 		file_is_lansan = False
@@ -10,6 +11,7 @@ for file in [f for f in os.listdir("纯词库") if os.path.isfile(f"纯词库/{f
 			file_is_lansan = True
 		for line in f:
 			word = line.strip()
+			union.add(word)
 			if word in dicts:
 				dicts[word] += 1
 			else:
@@ -21,3 +23,6 @@ result = [x for x in ls if dicts[x] > 3]
 
 with open('交集.txt', 'w', encoding='utf-8') as f:
 	f.write('\n'.join(sorted(result)))
+
+with open('并集.txt', 'w', encoding='utf-8') as f:
+	f.write('\n'.join(sorted(union)))
